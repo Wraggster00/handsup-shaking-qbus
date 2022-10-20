3f27daccaf031209764ad1f9be896e65d6bfa854
Wraggster00 <116266395+Wraggster00@users.noreply.github.com>
2022-10-20 10:28:38 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -9,7 +9,7 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(3)
 		if IsControlJustPressed(1, 323) then
-            if not handsup then
+    if not handsup then
                 TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 8.0, 8.0, -1, 50, 0, false, false, false)
                 handsup = true
                 TriggerServerEvent('stormydormy:server:toggleHandsup', true)
@@ -19,11 +19,19 @@ Citizen.CreateThread(function()
                 ClearPedSecondaryTask(PlayerPedId())
             end
         end
+        
 
         if handsup then
+            DisablePlayerFiring(GetPlayerPed(-1),true) -- Disable firing
+            DisableControlAction(0, 203, true) -- Disable aiming
+            DisableControlAction(0, 25, true) -- Disable aiming
+            DisableControlAction(0, 45, true) -- Disable reloading
+            DisableControlAction(0, 24, true) -- Disable aiming
+
+
             if not IsEntityPlayingAnim(PlayerPedId(), dict, "handsup_standing_base", 3) then
                 TaskPlayAnim(PlayerPedId(), dict, "handsup_standing_base", 8.0, 8.0, -1, 50, 1, false, false, false)
-            end
+end
         end
     end
 end)
